321ecd2eb4cf6e99f530416bb1609a9dae8e5019
jaehee6 <0jujh8@gmail.com>
2022-04-13 23:17:10 +0900

    Initial Commit

---
+++ atte22/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  atte22
+//
+//  Created by 이재희 on 2022/04/13.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
