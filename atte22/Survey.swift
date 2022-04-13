6a4404d39cf6e11a89c76ba5583654d723337e2b
jaehee6 <0jujh8@gmail.com>
2022-04-13 23:52:11 +0900

    .gitignore생성, Survey스토리보드 생성

---
+++ atte22/Survey.swift
@@ -0,0 +1,29 @@
+//
+//  Survey.swift
+//  atte22
+//
+//  Created by 이재희 on 2022/04/13.
+//
+
+import UIKit
+
+class Survey: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
